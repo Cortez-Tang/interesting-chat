a56fae4485061f406edce796913198d238328918
tangzhicheng <tangzhicheng@aegis-data.cn>
2020-03-20 10:09:08 +0800

    解决冲突

--- pages/map/map.vue
+++ pages/map/map.vue
@@ -2,9 +2,7 @@
 	<view class="map">
 		<!-- <map :latitude="location.latitude" :longitude="location.longitude"  :show-compass="true" :enable-3D="true" ></map> -->
 		<!-- <uni-popup ref="popup" type="bottom">底部弹出 Popup</uni-popup> -->
-		<web-view :src="url"></web-view>
-<!-- 		<web-view src="https://apis.map.qq.com/tools/poimarker?type=1&keyword=%e9%85%92%e5%ba%97&center=CurrentLocation&radius=1000&key=O5ABZ-XIXYD-JSB42-HZFQE-H2YRO-J7F2T&referer=趣聊-全部端"></web-view> -->
-<!-- 		<web-view src="https://apis.map.qq.com/tools/streetview?pano=10131053140812095958800&heading=7&pitch=-13&key=O5ABZ-XIXYD-JSB42-HZFQE-H2YRO-J7F2T&referer=趣聊-全部端"></web-view> -->
+		<web-view src="https://apis.map.qq.com/tools/locpicker?search=1&type=1&key=O5ABZ-XIXYD-JSB42-HZFQE-H2YRO-J7F2T&referer=趣聊-全部端"></web-view>
 	</view>
 </template>
 
